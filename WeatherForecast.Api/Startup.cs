f47fe349387a0490c988d80490f0f016cdf01be7
RyanTheShaw <numberonecharizard@gmail.com>
2021-12-12 22:14:10 -0500

    Base implementation of the controller api example
    - link to the project can be found here: https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-5.0&tabs=visual-studio

--- WeatherForecast.Api/Startup.cs
+++ WeatherForecast.Api/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WeatherForecast.Api.Models;
 
 namespace WeatherForecast.Api
 {
@@ -28,6 +30,9 @@ namespace WeatherForecast.Api
         {
 
             services.AddControllers();
+            services.AddDbContext<TodoContext>(opt =>
+                                               opt.UseInMemoryDatabase("TodoList"));
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WeatherForecast.Api", Version = "v1" });
